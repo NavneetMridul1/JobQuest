f2e8b92e874c7061aca9d6d766b17f9073d0b4fb
Navneet Kumar Mridul <navneet27july2000@gmail.com>
2025-07-27 02:54:06 +0530

    cross-origin fixed

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -5,6 +5,8 @@ export const sendToken = (user, statusCode, res, message) => {
   const options = {
     expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
     httpOnly: true,
+    secure: true, // important for HTTPS
+    sameSite: "None", // required for cross-origin cookies
   };
 
   res.status(statusCode).cookie("token", token, options).json({
